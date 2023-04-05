629d813eef42e7f8e0368d456468f344fb440efc
PAVAN903 <navap903@gmail.com>
2023-04-05 16:25:47 +0530

    Added:validated mail id

--- src/com/bridgelab/UserMain.java
+++ src/com/bridgelab/UserMain.java
@@ -9,7 +9,7 @@ public class UserMain {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter first name to validate");
 		String firstname=sc.next();
-		if(user.Validation(firstname)==true) {
+		if(user.Validationfirstname(firstname)==true) {
 			System.out.println("Valid firstname");
 		}else {
 			System.out.println("InValid firstname");
@@ -20,6 +20,13 @@ public class UserMain {
 			System.out.println("Valid lastname");
 		}else {
 			System.out.println("InValid laststname");
+		}
+		System.out.println("enter mailId to validate");
+		String mail=sc.next();
+		if(user.ValidationLastname(lastname)==true) {
+			System.out.println("Valid mail Id");
+		}else {
+			System.out.println("InValid mail Id");
 		}
 
 	}
--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -3,17 +3,25 @@ package com.bridgelab;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public boolean Validation (String firstname) {
-		Pattern pat=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
-		boolean matches=pat.matcher(firstname).matches();
+	public boolean Validationfirstname(String firstname) {
+		Pattern pat = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+		boolean matches = pat.matcher(firstname).matches();
 		return matches;
 	}
-	public boolean ValidationLastname (String lastname) {
-		Pattern pat=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
-		boolean matches=pat.matcher(lastname).matches();
+
+	public boolean ValidationLastname(String lastname) {
+		Pattern pat = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+		boolean matches = pat.matcher(lastname).matches();
 		return matches;
 	}
-	
-	
+
+	public boolean Validationmail(String mail) {
+		// E.g. abc.xyz@bl.co.in
+		// Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in)
+		Pattern pat = Pattern.compile(
+				"^[a-zA-Z0-9]+([!@#$%^&*()_+.,][a-zA-Z0-9]+)*[@][a-zA-Z]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$\");");
+		boolean matches = pat.matcher(mail).matches();
+		return matches;
+	}
 
 }
